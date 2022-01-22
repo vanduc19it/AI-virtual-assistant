c632bb51a52793c273602654ed756f57dadc92fe
phanvan2 <71816270+phanvan2@users.noreply.github.com>
2022-01-22 21:57:17 +0700

    Update handleData.py

--- src/public/handleData.py
+++ src/public/handleData.py
@@ -6,14 +6,14 @@ from tkinter import messagebox
 import handleFile
 
 # content : {"abc":"aaaa"} json
-def addData(content, url_file):
+def addData(content, url_file): # thêm dữ liệu
     try:
-        str = handleFile.readFile(url_file)    
-        contents = json.loads(str)
+        str = handleFile.readFile(url_file)    # lấy dữ liệu ban đầu ra 
+        contents = json.loads(str) # chuyển dữ liệu thành dạng json
         
-        contents.update(content)
-        content_data = json.dumps(contents,ensure_ascii=False)
-        handleFile.wirteFile(url_file, content_data)
+        contents.update(content) # cập nhật lại dữ liệu
+        content_data = json.dumps(contents,ensure_ascii=False) # chuyển dữ liệu về dạng String
+        handleFile.wirteFile(url_file, content_data) # ghi file
         return "success"
     except:
         return "error"
@@ -23,7 +23,7 @@ def deleteData(item_delete, url_file):
         str = handleFile.readFile(url_file)    
         contents = json.loads(str)
         
-        contents.pop(item_delete)
+        contents.pop(item_delete)# xóa dữ liệu giống với item_delete 
         content_data = json.dumps(contents,ensure_ascii=False)
         handleFile.wirteFile(url_file, content_data)
         return "success"
