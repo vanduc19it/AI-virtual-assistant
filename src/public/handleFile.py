69ecc66bfaf37acfe5cffa1678c1299c4bd9a631
phanvan2 <71816270+phanvan2@users.noreply.github.com>
2022-01-22 21:54:09 +0700

    Update handleFile.py

--- src/public/handleFile.py
+++ src/public/handleFile.py
@@ -1,21 +1,21 @@
 import io 
 import json
 
-def readFile(path):
+def readFile(path):# đọc file 
     try:
-        f_open = open(path, 'r', encoding='utf-8')
+        f_open = open(path, 'r', encoding='utf-8') # mở file và đọc 
         str = f_open.read()
         f_open.close
-        return str
+        return str # trả về chuỗi 
     except:
         print("err read file")
         return "error"
     
 
-def wirteFile(path, content ):
+def wirteFile(path, content ): # ghi file  với path là đường dẫn và content là nội dung cần ghi ( file sẽ ghi đè lên nội dung cũ
     try:
         print(content)
-        f_write = open(path, 'w+', encoding="utf8")
+        f_write = open(path, 'w+', encoding="utf8") 
         f_write.write(content)
         f_write.close
     except:
